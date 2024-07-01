8809b39d77b5e62ecb83ee0a42c2092678967472
pawanharry <wazirpawandeep23@gmail.com>
2024-07-01 16:32:02 -0400

    hero

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,11 +3,13 @@ import Navbar from './components/Navbar'
 import Banner from './components/Banner'
 import Footer from './components/Footer'
 import About from './components/About'
+import Hero from './components/Hero'
 
 const App = () => {
   return (
     <>
     <Navbar/>
+    <Hero/>
     <Banner/>
     <About/>
     <Footer/>
