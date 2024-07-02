c07c108a87350fb09b1d2e573d93987374d24adc
pawanharry <wazirpawandeep23@gmail.com>
2024-07-01 22:23:32 -0400

    modifications

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,30 +8,31 @@ import Price from './components/Price'
 import Portfolio from './components/Portfolio'
 import Reviews from './components/Reviews'
 import Contact from './components/Contact'
-import{ Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
+
 
 
 const App = () => {
   return (
-    <><Routes>
-      <Route path='/' element={<Hero/>}/>
-      <Route path='/Navbar' element={<Navbar/>}/>
-      <Route path='/AboutUs' element={<Banner/>}/>
-      <Route path='/Services' element={<About/>}/>
-      <Route path='/Price' element={<Price/>}/>
-      <Route path='/Portfolio' element={<Portfolio/>}/>
-      <Route path='/Reviews' element={ <Reviews/>}/>
-      <Route path='/Contact' element={  <Contact/>}/>
-      <Route path='/Footer' element={ <Contact/>}/>
+    <>
+    <Routes>
+            <Route path='/' element={<Hero />}/>
+            <Route path='/about' element={ <Banner/>}/>
+            <Route path='/services' element={<About/>}/>
+            <Route path='/Price' element={ <Price/>}/>
+            <Route path='/portfolio' element={ <Portfolio/>}/>
+            <Route path='/reviews' element={<Reviews/>} />
+            <Route path='/contact' element={<Contact/>} />
+
+    </Routes>
    
     
     
     
     
-   
+    
     
     <Footer/>
-    </Routes>
     </>
   )
 }
---
+++ Frontend/src/Home/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Banner from './components/Banner'
+import Footer from './components/Footer'
+import About from './components/About'
+import Hero from './components/Hero'
+import Price from './components/Price'
+import Portfolio from './components/Portfolio'
+import Reviews from './components/Reviews'
+import Contact from './components/Contact'
+
+
+function Home() {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
